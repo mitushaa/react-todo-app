80007278f9d22cfa93b184461f3507b398cfbd30
mitushaa <aryamitusha@gmail.com>
2020-10-13 13:51:33 +0530

    Updates

---
+++ precache-manifest.47480dddf5562cf3d80145b7ccbe7917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb003ba81df013de31175648bd2ca1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9a731119d1efd5cc8fb",
+    "url": "/static/css/main.a2f0b474.chunk.css"
+  },
+  {
+    "revision": "c19dd7c3867db50f0994",
+    "url": "/static/js/2.91f397c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.91f397c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a731119d1efd5cc8fb",
+    "url": "/static/js/main.6d5d6dd5.chunk.js"
+  },
+  {
+    "revision": "eb323c622e106faecb0e",
+    "url": "/static/js/runtime-main.341c860d.js"
+  }
+]);
